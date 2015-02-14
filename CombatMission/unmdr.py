ba10df2c93786203a11ef67c240845940d39c1d5
Stan Bobovych <stan.bobovych@gmail.com>
2015-02-13 22:49:54 -0800

    Added extension histogram to filepie

--- CombatMission/unmdr.py
+++ CombatMission/unmdr.py
@@ -1,28 +1,43 @@
 import struct
+import sys
 
-#filepath = ".\\out\\terrain\\flavor objects\\bin\\bin2.mdr"
-#filepath = ".\\out\\terrain\\flavor objects\\bin\\bin1.mdr"
-filepath = ".\\out\\vehicles\jeep\jeep-unarmed.mdr"
-#vertex_offset = 0xBE
-vertex_offset = 0xB8
-print "mdr model"
+
+
+#filepath = ".\\Normandy Demo v100\\vehicles\\jeep\\jeep-unarmed.mdr"
+#filepath = "ak-74.mdr"
+filepath = "rpg-7v1-lod-5.mdr"
+#filepath = "rpg-7v1.mdr"
 
 with open(filepath, "rb") as f:
-    f.seek(vertex_offset)
-    for j in range(0,1):
-        f.read(4)
-        vertex_count, = struct.unpack("<I", f.read(4))
-        print "Vertex count:", vertex_count
-        for i in range(0, vertex_count/3):
-            v0, v1, v2 = struct.unpack("<HHH", f.read(6))
-            #print v0,v1,v2
-        print hex(f.tell())
-        verts_in_section, = struct.unpack("<I", f.read(4))
-        print "Verts in section:", verts_in_section/5    
-        for i in range(0, verts_in_section/5):
-            #print hex(f.tell())
-            f1,f2,f3 = struct.unpack("<fff", f.read(12))            
-            u,v = struct.unpack("<ff", f.read(8))
-            print "C", f1,f2,f3
-            #print u,v
+    num_models,name_length = struct.unpack("<IxH", f.read(7))
+    #print "number of models", num_models
+    #print "name length", name_length
+    model_class = f.read(name_length)
+    #print "model class", model_class
+    f.read(1) # always 2?
+    for i in range(0, 44):
+        unk, = struct.unpack("f", f.read(4))
+        #print unk
     
+    vertex_count, = struct.unpack("<I", f.read(4))
+    #print "Face count:", vertex_count/3
+
+    for i in range(0, vertex_count/3):
+        v0, v1, v2 = struct.unpack("<HHH", f.read(6))
+        #print "f",v0+1,v1+1,v2+1
+    
+    uv_in_section, = struct.unpack("<I", f.read(4))
+    #print "UV in section:", uv_in_section
+    #print hex(f.tell())    
+    
+    for i in range(0, uv_in_section/2):     
+        u,v = struct.unpack("<ff", f.read(8))        
+        print "v", u,v,0
+        
+        
+    unk, = struct.unpack("<I", f.read(4))
+    for i in range(0, unk):
+        print struct.unpack("f", f.read(4))
+    
+    print "end of object", hex(f.tell())
+
--- common/filepie.py
+++ common/filepie.py
@@ -19,13 +19,14 @@ import argparse
 import os
 import re
 from pylab import *
-
+import sys
 
 parser = argparse.ArgumentParser(description='Tool that recursively scans directories and visualizes file sizes.', \
                                 epilog='')
 
 parser.add_argument('path', nargs='?', help='Root directory to scan.')
 parser.add_argument('--filt', default='', help='Regex expression used to filter out directories or files.')
+parser.add_argument('--ext-only', default=False, action='store_true', help='Only do file extension analysis')
 
 args = parser.parse_args()
 path = args.path
@@ -34,22 +35,32 @@ filt = re.compile(args.filt)
 num_files = 0
 total_size = 0
 file_dict = {}
+ext_dict = {}
 for root, dir, files in os.walk(path):
     if re.search(filt, root) == None or args.filt == '':
         for f in files:
-            fpath = os.path.join(root,f)
-            num_files += 1
-            fsize = os.path.getsize(fpath)
-            total_size += fsize
-            file_dict[fpath] = fsize
+            if not args.ext_only:
+                fpath = os.path.join(root,f)
+                num_files += 1
+                fsize = os.path.getsize(fpath)
+                total_size += fsize
+                file_dict[fpath] = fsize
 
-files_counts = sorted(file_dict.items(), key=lambda x:x[1], reverse=True)     # list of tuples (path, size)
-paths,sizes = zip(*files_counts)
+            ext = os.path.splitext(f)[1]
+            if ext_dict.has_key(ext):
+                ext_dict[ext] = ext_dict[ext] + 1
+            else:
+                ext_dict[ext] = 1
 
-# plotting
-labels = map(lambda x: os.path.basename(x), paths)
-figure(1, figsize=(6,6))
-ax = axes([0.1, 0.1, 0.8, 0.8])
-pie(sizes, labels=labels)
-title("Directory: %s, file count: %s, total size(mb): %s" % (path, num_files, total_size/1024))
-show()
+print ext_dict
+if not args.ext_only:    
+    files_counts = sorted(file_dict.items(), key=lambda x:x[1], reverse=True)     # list of tuples (path, size)
+    paths,sizes = zip(*files_counts)
+
+    # plotting
+    labels = map(lambda x: os.path.basename(x), paths)
+    figure(1, figsize=(6,6))
+    ax = axes([0.1, 0.1, 0.8, 0.8])
+    pie(sizes, labels=labels)
+    title("Directory: %s, file count: %s, total size(mb): %s" % (path, num_files, total_size/1024))
+    show()
