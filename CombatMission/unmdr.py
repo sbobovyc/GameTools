4c7218e390bab0f89b0cc103d49dca2941113b21
Stan Bobovych <stan.bobovych@gmail.com>
2015-02-21 01:14:53 -0800

    unmdr script can now dump faces and vertices of ak74

--- CombatMission/unmdr.py
+++ CombatMission/unmdr.py
@@ -1,13 +1,7 @@
 import struct
 import sys
 
-
-
-#filepath = ".\\Normandy Demo v100\\vehicles\\jeep\\jeep-unarmed.mdr"
-#filepath = "ak-74.mdr"
-filepath = "rpg-7v1-lod-5.mdr"
-#filepath = "rpg-7v1.mdr"
-
+filepath = "ak-74-lod-2.mdr"
 
 ########
 # perhaps:
@@ -30,33 +24,77 @@ with open(filepath, "rb") as f:
     model_class = f.read(name_length)
     #print "model class", model_class
     f.read(1) # always 2?
-    for i in range(0, 44):
+    for i in range(0, 0xB0/4):
         unk, = struct.unpack("f", f.read(4))
         #print unk
-    print hex(f.tell())
-    vertex_count, = struct.unpack("<I", f.read(4))
-    print "Face count:", vertex_count/3
+    print "#Finished unknown section", hex(f.tell())
+    print "#Maybe start face vertex indices"
+    face_count, = struct.unpack("<I", f.read(4))
+    print "#Face count:", face_count/3
 
-    for i in range(0, vertex_count/3):
+    for i in range(0, face_count/3):
         v0, v1, v2 = struct.unpack("<HHH", f.read(6))
         print "f",v0+1,v1+1,v2+1
+    print "#Finished face vertex indices", hex(f.tell())
     
+
+    print "#Maybe start UVs"
     uv_in_section, = struct.unpack("<I", f.read(4))
-    #print "UV in section:", uv_in_section
-    #print hex(f.tell())    
+    print "#UV in section:", uv_in_section
+    
     
     for i in range(0, uv_in_section/2):     
         u,v = struct.unpack("<ff", f.read(8))        
         #print "v", u,v,0
-        
-        
-    unk, = struct.unpack("<I", f.read(4))
-    print unk
-    for i in range(0, unk):
-        print i,struct.unpack("f", f.read(4))
+    print "#Finish UV section:", hex(f.tell())
     
-    print "end of object", hex(f.tell())
 
-# ak-74.mdr
-# offset 0x27cf is number of floats (num/3 = vertices), followed by x,y,z
-# rifle, unknown, then magazine model
+    print "#Start unknown section"    
+    unk, = struct.unpack("<I", f.read(4))
+    print "#Unknown",unk    
+    for i in range(0, unk/16):
+        print "#",i,struct.unpack("ff", f.read(8))
+    
+    print "#End unknown section", hex(f.tell())
+
+    f.read(4) # 0
+    f.read(4) # 2
+
+    name_length, = struct.unpack("<H", f.read(2))
+    model_class = f.read(name_length)
+    print "#Some matrix?", model_class
+    for i in range(0, 0x30/4):
+        print "#",i,struct.unpack("f", f.read(4))
+    print "#End matrix", hex(f.tell())
+
+    name_length, = struct.unpack("<H", f.read(2))
+    model_class = f.read(name_length)
+    print "#Some matrix?", model_class
+    for i in range(0, 0x30/4):
+        print "#",i,struct.unpack("f", f.read(4))
+    print "#End matrix", hex(f.tell())
+
+    print "#Start some unknown"
+    for i in range(0, 26):
+        print "#",i,struct.unpack("f", f.read(4))
+    print "#End unknown", hex(f.tell())
+
+    f.read(4) # 0
+    name_length, = struct.unpack("<H", f.read(2))
+    model_class = f.read(name_length)
+    print "#Model name", model_class
+
+    f.read(1) # always 2?
+    for i in range(0, 0xB0/4):
+        unk, = struct.unpack("f", f.read(4))
+        #print unk
+    print "#Finished unknown section", hex(f.tell())
+
+    print "#Start vertices"
+    vertex_floats, = struct.unpack("<I", f.read(4))
+    print "#Vertices", vertex_floats/3
+    for i in range(0, vertex_floats/3):        
+        x,y,z = struct.unpack("fff", f.read(12))
+        print "v",x,y,z
+    print "#End vertices", hex(f.tell())
+    
