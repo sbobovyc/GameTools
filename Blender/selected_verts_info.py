c3c20d1f83e1e40e4e8be85aaee88c56080b1a64
Stan Bobovych <stan.bobovych@gmail.com>
2015-10-13 22:09:32 -0400

    Added Blender script that prints out vertex group info for selected vertices

---
+++ Blender/selected_verts_info.py
@@ -0,0 +1,30 @@
+""" @author Stanislav Bobovych
+@detail Tested with Blender 2.75
+"""
+import bpy
+
+print("\n"*5)
+bpy.ops.object.mode_set(mode = 'OBJECT')
+if bpy.context.object.type == "MESH":    
+    object = bpy.context.object
+    # print all vertex groups for each object
+    print(object.vertex_groups.keys())
+    for group in object.vertex_groups:
+        print("Group %i, name %s" % (group.index, group.name))
+
+    for modifier in object.modifiers:
+        if modifier.type == 'ARMATURE' and modifier.object:
+            armature = modifier.object.data
+
+            for bone in armature.bones:
+                if bone.use_deform and bone.name in object.vertex_groups:
+                    print("'%s' used for armature deformation" % (bone.name))
+                    
+    mesh = bpy.context.object.data
+    selected_verts = [v for v in mesh.vertices if v.select]
+    for v in selected_verts:
+        print("Vertex index: %i," % v.index, "Coordinate:", v.co)
+        for g in v.groups:
+            print("\tGroup: %i," % g.group, object.vertex_groups[g.group].name, "Weight:", g.weight)
+else:
+    print("Failed to select mesh")
