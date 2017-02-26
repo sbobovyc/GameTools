c03797b6d7a263210bab980f2796e38e12cd390e
Stan Bobovych <stan.bobovych@gmail.com>
2017-02-26 01:50:58 -0500

    Added printing of vertex texture coordinates

--- Blender/selected_verts_info.py
+++ Blender/selected_verts_info.py
@@ -26,5 +26,14 @@ if bpy.context.object.type == "MESH":
         print("Vertex index: %i," % v.index, "Coordinate:", v.co)
         for g in v.groups:
             print("\tGroup: %i," % g.group, object.vertex_groups[g.group].name, "Weight:", g.weight)
+            
+    selected_faces = [f for f in mesh.polygons if f.select]
+    uv_layer = mesh.uv_layers.active.data
+    for f in selected_faces:
+        print("Face index: %i," % f.index, "vertex indices:", f.vertices[:])
+        for li in f.loop_indices:
+            vi = mesh.loops[li].vertex_index
+            uv = uv_layer[li].uv
+            print("    Loop index %i (Vertex %i) - UV %f %f" % (li, vi, uv.x, uv.y))    
 else:
     print("Failed to select mesh")
