99343109ab28a50b96ba46cf09c226a683abd2c4
Stan Bobovych <stan.bobovych@gmail.com>
2017-03-08 00:22:53 -0500

    raw2obj can now output vertex indices and texture coordinates in addition to vertex positions

--- common/raw2obj.py
+++ common/raw2obj.py
@@ -20,28 +20,50 @@ import sys
 import struct
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Tool that\'s used to read apitrace raw data dumps of vertex data and print wavefront obj vertices. It can also be used with any other raw data.',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(
+        description='Tool that\'s used to read apitrace raw data dumps of vertex data and print wavefront obj vertices. It can also be used with any other raw data.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('file', nargs='?', help='Raw file')
+    parser.add_argument('--type', required=True, choices=['indices', 'positions', 'texcoords'])
     parser.add_argument('-o', '--offset', default="0x0", help='Offset into file, in hex')
-    parser.add_argument('-c', '--count', type=int, default=sys.maxint, help='Number of vertices')
-    
-    args = parser.parse_args()    
+    parser.add_argument('-c', '--count', type=int, default=sys.maxsize, help='Number of vertices')
+
+    args = parser.parse_args()
     path = args.file
     offset = int(args.offset, 16)
     count = args.count
-    if count != sys.maxint:
-        count = args.count*3*4
-        
+    if count != sys.maxsize:
+        if args.type == "indices":
+            count = args.count * 3 * 2
+        elif args.type == "positions":
+            count = args.count * 3 * 4
+        elif args.type == "texcoords":
+            count = args.count * 2 * 4
+
     byte_count = 0
     with open(path, 'rb') as f:
         f.seek(offset)
         while byte_count < count:
-            buf = f.read(3*4)
-            if not buf: break
-            f1,f2,f3 = struct.unpack("fff", buf)
-            print "v",f1,f2,f3
-            byte_count += 3*4
+            if args.type == "indices":
+                buf = f.read(3 * 2)  # assume vertex indices are unsigned shorts
+                if not buf:
+                    break
+                v1, v2, v3 = struct.unpack("HHH", buf)
+                print("f %i/%i %i/%i %i/%i" % (v1+1, v1+1, v2+1, v2+1, v3+1, v3+1))
+                byte_count += 3 * 2
+            elif args.type == "positions":
+                buf = f.read(3 * 4)
+                if not buf:
+                    break
+                f1, f2, f3 = struct.unpack("fff", buf)
+                print("v", f1, f2, f3)
+                byte_count += 3 * 4
+            elif args.type == 'texcoords':
+                buf = f.read(2 * 4)  # assume texture coordinates are floats
+                if not buf:
+                    break
+                u, v, = struct.unpack("ff", buf)
+                print("vt", u, v)
+                byte_count += 2 * 4
 
-    print "# number of bytes", byte_count
-
+    print("# number of bytes", byte_count)
